68d2f67acc620cca79724ddd9c365a8ec963291f
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-03-19 06:47:59 +0300

    create files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+fun main() {
+  var names = arrayOf("Jane", "Lucy", "Ann", "Faith")
+  println(names)
+  var words = arrayOf("Tom","Alice","Faith","Scarlet")
+  println(words)
+  println(names.get(2))
+  println(names[2])
+  println(names.indexOf("Ann"))
+  println(names.contentToString())
+   names = names.plus("Waluse")
+   names = names.plus(arrayOf("Nairat","Linet","Agnes","Warga","Hannah"))
+  println(names.contentToString())
+  println(names.contentToString())
+  println(names.contentToString())
+
+  var numbers= arrayOf(12,12,13)
+println(numbers.get(1)+(2))
+
+
+
+
+
+  var country = arrayOf("Kenya" ,10 , true ,"Scarlet")
+  println(country[1])
+  println(country.indexOf("Scarlet"))
+  println(country.get(2))
+name(arrayOf("hello","jane","nany"))
+
+}
+fun name(names:Array<String>){
+  println(names.contentToString())
+
+}
+fun word(words:Array<String>){
+  println(words.contentToString())
+}
+
+
+
